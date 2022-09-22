dc40aaff90a8317ec1ba793eabd5cab44ea889af
liuguang <fmj_lg@163.com>
2022-09-22 21:23:09 +0800

    完成日志系统核心功能

--- api/V1/log/log_api.go
+++ api/V1/log/log_api.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	logRequest "kuke_logger/common/request"
 	"kuke_logger/common/response"
@@ -11,15 +12,19 @@ type LogApi struct {
 
 func (l *LogApi) CreateLonger(c *gin.Context) {
 	//data, _ := ioutil.ReadAll(c.Request.Body)
-	//fmt.Printf(string(data))
 	//filePath := "./log.json"
-	//file, _ := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 066)
+	//file, _ := os.OpenFile(filePath, os.O_CREATE|os.O_RDWR, 777)
 	//defer file.Close()
 	//write := bufio.NewWriter(file)
-	//write.WriteString(string(data) + "/n")
+	//_, err := write.WriteString(string(data) + "\n")
+	//fmt.Println(string(data))
+	//if err != nil {
+	//	log.Println(err.Error())
+	//}
+	//write.Flush()
 	var log []logRequest.LogRequest
 	_ = c.ShouldBindJSON(&log)
-	//fmt.Print(len(log))
+	fmt.Print(len(log))
 	_ = logService.CreateLog(log)
 	response.Result(1, "{}", "success", c)
 }
