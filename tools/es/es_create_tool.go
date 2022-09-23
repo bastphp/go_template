8bce48f7bc30093fd85505b4c3b6d4083cac82b4
liuguang <fmj_lg@163.com>
2022-09-23 18:25:44 +0800

    删除筛选时间

--- tools/es/es_create_tool.go
+++ tools/es/es_create_tool.go
@@ -2,21 +2,15 @@ package es
 
 import (
 	"context"
-	"fmt"
 	"kuke_logger/common/request"
 	"kuke_logger/global"
 	"log"
-	"time"
 )
 
 type CreateTool struct {
 }
 
 func (ct *CreateTool) InES(indexName string, logs request.LogRequest) string {
-	now := time.Now()
-	h, _ := time.ParseDuration("-1h")
-	timeStamp := now.Add(8 * h).Format("2006-01-02 15:01:05")
-	fmt.Println(timeStamp)
 	msg := request.LogMapping{
 		KkRequestId: logs.Messages.KkRequestId,
 		Kubernetes: request.KubernetesLog{
@@ -41,7 +35,6 @@ func (ct *CreateTool) InES(indexName string, logs request.LogRequest) string {
 		ResponseTime: logs.Messages.ResponseTime,
 		Tag:          logs.Messages.Tag,
 		UserId:       logs.Messages.UserId,
-		TimeStamp:    timeStamp,
 	}
 	res, err := global.GVA_ES.Index().Index(indexName).BodyJson(msg).Do(context.Background())
 	if err != nil {
